66155f490e16f3dd2c935627324a4c1375a288d6
AndroidProjects <payalvarade@gmail.com>
2020-01-04 16:54:43 +0530

    web view with card view

---
+++ app/src/main/java/com/example/mywebapplication/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.mywebapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView webView =findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient());
+
+        webView.loadUrl("http://chetanketan.freeoda.com/shamshaan/index.php");
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+    }
+}
